88b53602fc649fe575d5a6a7f407b7e9bffedac8
Nicolas Coronado <nicolasdgioc15@gmail.com>
2024-08-10 13:55:15 -0600

    proyecto finalizado

--- django_tasks/settings.py
+++ django_tasks/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_tasks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tasksdb',
+        'USER': 'nicklauss',
+        'PASSWORD': 'coxs223',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
